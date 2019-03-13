29a18a825cdde1d43656b25f94251b5a3a740b08
ThalaHemanth <viruhemanth@gmail.com>
2019-03-13 20:58:47 +0530

    Heroki error

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,11 +1,11 @@
 const Query = {
   hello: (parent, args, { db }) => {
-  //   db.collection('cities')
-  //     .doc('LA')
-  //     .delete()
-  //     .then(() => console.log('Docs Deleted Successfully'));
-  // }
-  return "Hello Worrld"
+    //   db.collection('cities')
+    //     .doc('LA')
+    //     .delete()
+    //     .then(() => console.log('Docs Deleted Successfully'));
+    // }
+    return 'Hello Worrld';
+  }
 };
-
 export { Query as default };
